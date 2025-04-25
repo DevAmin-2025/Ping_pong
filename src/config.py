11f1e3f4109e9f7ef6dbe80fa045efc9bd7c531d
Amin Mohamadi <amindevzone@gmail.com>
2025-04-25 08:52:19 +0330

    Create config.py file and write initial attributes for the game

---
+++ src/config.py
@@ -0,0 +1,33 @@
+# Screen dimentions
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+
+# Names showed for players
+player1_name = 'A'
+player2_name = 'B'
+
+# Colors used in the game
+RED = '#C2185B'
+BLUE = '#1976D2'
+GREEN = '#388E3C'
+
+# Common paddle attrs
+PADDLE_WIDTH = 10
+PADDLE_HEIGHT = 100
+PADDLE_SPEED = 7
+paddle_y = (SCREEN_HEIGHT // 2) - (PADDLE_HEIGHT // 2)
+
+# Paddle2
+PADDLE2_X = 10
+
+# Paddle1
+PADDLE1_X = SCREEN_WIDTH - (PADDLE_WIDTH + 10)
+
+# Ball
+BALL_WIDTH = 30
+BALL_HEIGHT = 30
+ball_x = (SCREEN_WIDTH // 2) - (BALL_WIDTH // 2)
+ball_y = (SCREEN_HEIGHT // 2) - (BALL_HEIGHT // 2)
+
+# One-second delay at the begining
+initial_start_delay = 1000 #ms
